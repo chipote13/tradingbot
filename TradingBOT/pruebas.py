16b9b599c0ff90ff9def6a98657282d932270ac2
Daniel <dcgandara@gmail.com>
2022-06-28 10:27:16 -0500

    otra vez

---
+++ TradingBOT/pruebas.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from pandas_datareader import data
+from pandas_datareader._utils import RemoteDataError
+import requests
+import matplotlib.pyplot as plt
+import numpy as np
+from datetime import datetime, timedelta
+import yfinance as yf
